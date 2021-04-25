8bc2762cb80f12153596b9c0d984522c062b7d1d
Gabriel Berto <gabriel.berto@pottencial.com.br>
2021-04-25 14:53:18 -0300

    Update brew installation script

--- .config/yadm/bootstrap.d/brew.sh
+++ .config/yadm/bootstrap.d/brew.sh
@@ -3,11 +3,12 @@
 # install homebrew if it's missing
 if ! command -v brew >/dev/null 2>&1; then
 	echo "Installing homebrew"
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 if [ -f "$HOME/.Brewfile" ]; then
 	echo "Updating homebrew bundle"
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 	brew bundle --global
 fi
 
