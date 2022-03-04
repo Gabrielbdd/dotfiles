c328f5c5e38bcf1e8c973378dad1451d2324368c
Gabriel Berto <gabriel.berto@pottencial.com.br>
2022-03-04 19:01:38 -0300

    Update fish configs

--- private_dot_config/private_fish/user_config.fish
+++ private_dot_config/private_fish/user_config.fish
@@ -7,5 +7,11 @@ fish_add_path -m /usr/local/bin
 fish_vi_key_bindings
 set fish_cursor_replace_one underscore
 
+set -gx PNPM_HOME "/home/gabriel/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+
+alias lg lazygit
+
 # See why it makes the start freeze
-# podman completion fish | source
+podman completion fish | source
+
